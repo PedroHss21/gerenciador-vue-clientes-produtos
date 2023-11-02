390f7c56f5f396121ecd5b57c2839c2c1fef0658
Eduardo Esplinio <esplinioo@gmail.com>
2023-11-02 11:42:02 -0300

    gerencia sessão de usuário - `Logout`

---
+++ src/views/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav>
+      <button @click="performLogout">Logout</button>
+    </nav>
+  </template>
+  
+  <script>
+  import { mapActions } from 'vuex';
+  
+  export default {
+    methods: {
+      ...mapActions(['logoutUser']),
+  
+      performLogout() {
+        this.logoutUser()
+          .then(() => {
+            this.$router.push('/login');
+          })
+          .catch(error => {
+            console.error('Erro no logout:', error);
+          });
+      }
+    }
+  };
+  </script>
+  
