f4e60a98c5f69ffa37b3626ca3d78983aedcee6f
Eduardo Esplinio <esplinioo@gmail.com>
2023-11-04 02:09:58 -0300

    implementa títulos para páginas

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,11 +6,11 @@
     <v-row align="center">
       <v-col cols="12" sm="auto" class="d-flex align-items-center">
         <v-icon large color="#00609c">mdi-home</v-icon>
-        <h2 class="ml-2 mb-0 font-weight-normal message-color">Principal</h2>
+        <h2 class="ml-2 mb-0 font-weight-normal primary--text">Principal</h2>
       </v-col>
     </v-row>
     
-    <v-divider class="my-3" :style="{ 'backgroundColor': '#a2a9ad' }"></v-divider>
+    <v-divider class="my-3" :style="{ 'backgroundColor': 'tertiary' }"></v-divider>
   </v-container>
 </template>
 
@@ -36,9 +36,6 @@ export default {
 </script>
 
 <style scoped>
-.message-color {
-  color: #00609c; /* Define a cor do texto */
-}
 
 .font-weight-normal {
   font-weight: normal; /* Remove o negrito do texto */
