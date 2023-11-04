6c15eba241d48c6a7dd1b64d441fe518cac37041
Eduardo Esplinio <esplinioo@gmail.com>
2023-11-04 01:08:39 -0300

    personaliza layout

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
-    <AppFooter />
-  </div>
+  <v-container>
+    
+    <div class="mt-5"></div>
+
+    <v-row align="center">
+      <v-col cols="12" sm="auto" class="d-flex align-items-center">
+        <v-icon large color="#00609c">mdi-home</v-icon>
+        <h2 class="ml-2 mb-0 font-weight-normal message-color">Principal</h2>
+      </v-col>
+    </v-row>
+    
+    <v-divider class="my-3" :style="{ 'backgroundColor': '#a2a9ad' }"></v-divider>
+  </v-container>
 </template>
+
 
 <script>
 import { mapActions } from 'vuex';
@@ -24,5 +35,12 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.message-color {
+  color: #00609c; /* Define a cor do texto */
+}
+
+.font-weight-normal {
+  font-weight: normal; /* Remove o negrito do texto */
+}
 </style>
