f2b78c814c377c290140a61ac7685d5f5112001f
Eduardo Esplinio <esplinioo@gmail.com>
2023-11-07 07:56:03 -0300

    btn para Fechar e logo no `Footer`

--- src/components/products/ProductTable.vue
+++ src/components/products/ProductTable.vue
@@ -1,216 +1,220 @@
 <template>
-    <v-row justify="center">
-      <v-col cols="12" md="10" lg="12">
-        <v-container>
+  <v-row justify="center">
+    <v-col cols="12" md="10" lg="12">
+      <v-container>
+
+        <!-- Espaçamento no topo -->
+        <div class="mt-5"></div>
+
+        <!-- Título Produtos com ícone -->
+        <v-row align="center">
+          <v-col cols="12" sm="auto" class="d-flex align-items-center">
+            <v-icon large color="primary">mdi-package</v-icon>
+            <h2 class="ml-2 mb-0 font-weight-normal primary--text">Produtos</h2>
+          </v-col>
+        </v-row>
+
+        <!-- Linha horizontal personalizada -->
+        <v-divider class="my-3" :style="{ 'backgroundColor': 'tertiary' }"></v-divider>
+
+        <!-- Botão de Novo Produto -->
+        <v-btn class="action-button mb-7 mt-5" color="primary" @click="openNewProductDialog">
+          Novo Produto
+        </v-btn>
+        <product-manager ref="productManager"></product-manager>
+
+        <!-- Tabela de Produtos -->
+        <v-data-table :headers="headers" :items="produtos" :loading="loading" class="elevation-1">
+          <template v-slot:item="{ item }">
+            <tr>
+              <td>{{ item.id }}</td>
+              <td>{{ item.nome }}</td>
+              <td>{{ item.valor | currency }}</td>
+              <td>{{ item.quantidadeEstoque }}</td>
+              <td>{{ item.observacao }}</td>
+              <td>{{ item.dataCadastro | formatDate }}</td>
+              <td>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon v-bind="attrs" v-on="on" @click="editarProduto(item)">
+                      <v-icon color='primary'>mdi-pencil-outline</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Editar Produto</span>
+                </v-tooltip>
+
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon v-bind="attrs" v-on="on" @click="openDeleteDialog(item)">
+                      <v-icon color='primary'>mdi-delete-outline</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Excluir Produto</span>
+                </v-tooltip>
+
+              </td>
+            </tr>
+          </template>
+        </v-data-table>
+        <!-- Diálogo de Confirmação de Exclusão -->
+        <v-dialog v-model="deleteDialog" persistent max-width="500px">
+          <v-card>
+            <v-card-title class="d-flex justify-space-between">
+              Excluir Produto
+              <!-- Botão de fechar no canto superior direito -->
+              <v-btn icon @click="closeDeleteDialog">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </v-card-title>
+            <v-card-text>
+              Você tem certeza que deseja excluir o produto <strong>{{ selectedProduto?.nome }}</strong>?
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn class='delete-button' text @click="deletarProduto">Excluir Produto</v-btn>
+              <v-btn class='cancel-button' text @click="closeDeleteDialog">Cancelar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-container>
+    </v-col>
+  </v-row>
+</template>
   
-          <!-- Espaçamento no topo -->
-          <div class="mt-5"></div>
-  
-          <!-- Título Produtos com ícone -->
-          <v-row align="center">
-            <v-col cols="12" sm="auto" class="d-flex align-items-center">
-              <v-icon large color="primary">mdi-package</v-icon>
-              <h2 class="ml-2 mb-0 font-weight-normal primary--text">Produtos</h2>
-            </v-col>
-          </v-row>
-          
-          <!-- Linha horizontal personalizada -->
-          <v-divider class="my-3" :style="{ 'backgroundColor': 'tertiary' }"></v-divider>
-  
-          <!-- Botão de Novo Produto -->
-          <v-btn class="action-button mb-7 mt-5" color="primary" @click="openNewProductDialog">
-            Novo Produto
-          </v-btn>
-          <product-manager ref="productManager"></product-manager>
-  
-          <!-- Tabela de Produtos -->
-          <v-data-table
-            :headers="headers"
-            :items="produtos"
-            :loading="loading"
-            class="elevation-1"
-          >
-            <template v-slot:item="{ item }">
-              <tr>
-                <td>{{ item.id }}</td>
-                <td>{{ item.nome }}</td>
-                <td>{{ item.valor | currency }}</td>
-                <td>{{ item.quantidadeEstoque }}</td>
-                <td>{{ item.observacao }}</td>
-                <td>{{ item.dataCadastro | formatDate }}</td>
-                <td>
-                  <v-tooltip bottom>
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn icon v-bind="attrs" v-on="on" @click="editarProduto(item)">
-                        <v-icon color='primary'>mdi-pencil-outline</v-icon>
-                      </v-btn>
-                    </template>
-                    <span>Editar Produto</span>
-                  </v-tooltip>
-  
-                  <v-tooltip bottom>
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn icon v-bind="attrs" v-on="on" @click="openDeleteDialog(item)">
-                        <v-icon color='primary'>mdi-delete-outline</v-icon>
-                      </v-btn>
-                    </template>
-                    <span>Excluir Produto</span>
-                  </v-tooltip>
-  
-                </td>
-              </tr>
-            </template>
-          </v-data-table>
-          <!-- Diálogo de Confirmação de Exclusão -->
-          <v-dialog v-model="deleteDialog" persistent max-width="500px">
-            <v-card>
-              <v-card-title>Excluir Produto</v-card-title>
-              <v-card-text>
-                Você tem certeza que deseja excluir o produto <strong>{{ selectedProduto?.nome }}</strong>?
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn class='delete-button' text @click="deletarProduto">Excluir Produto</v-btn>
-                <v-btn class='cancel-button' text @click="closeDeleteDialog">Cancelar</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-container>
-      </v-col>
-    </v-row>
-  </template>
-  
-  <script>
-  import { mapActions, mapState } from 'vuex';
-  import ProductManager from '@/views/ProductManager.vue';
-  
-  export default {
-    components: {
-      ProductManager
+<script>
+import { mapActions, mapState } from 'vuex';
+import ProductManager from '@/views/ProductManager.vue';
+
+export default {
+  components: {
+    ProductManager
+  },
+  data() {
+    return {
+      deleteDialog: false,
+      selectedProduto: null,
+      loading: false,
+      headers: [
+        { text: 'ID', value: 'id' },
+        { text: 'Nome', value: 'nome' },
+        { text: 'Valor', value: 'valor' },
+        { text: 'Estoque', value: 'quantidadeEstoque' },
+        { text: 'Observação', value: 'observacao' },
+        { text: 'Data de Cadastro', value: 'dataCadastro', sortable: false },
+        { text: 'Ações', value: 'actions', sortable: false },
+      ],
+    };
+  },
+  computed: {
+    ...mapState(['produtos']),
+  },
+  methods: {
+    ...mapActions(['fetchProdutos', 'deleteProduto']),
+    openNewProductDialog() {
+      this.$refs.productManager.openDialog();
     },
-    data() {
-      return {
-        deleteDialog: false,
-        selectedProduto: null,
-        loading: false,
-        headers: [
-          { text: 'ID', value: 'id' },
-          { text: 'Nome', value: 'nome' },
-          { text: 'Valor', value: 'valor' },
-          { text: 'Estoque', value: 'quantidadeEstoque' },
-          { text: 'Observação', value: 'observacao' },
-          { text: 'Data de Cadastro', value: 'dataCadastro', sortable: false },
-          { text: 'Ações', value: 'actions', sortable: false },
-        ],
-      };
+    editarProduto(produto) {
+      this.$refs.productManager.openDialog(true);
+      this.$nextTick(() => {
+        this.$refs.productManager.produto = { ...produto };
+      });
     },
-    computed: {
-      ...mapState(['produtos']),
+    openDeleteDialog(produto) {
+      this.selectedProduto = produto;
+      this.deleteDialog = true;
     },
-    methods: {
-      ...mapActions(['fetchProdutos', 'deleteProduto']),
-      openNewProductDialog() {
-        this.$refs.productManager.openDialog();
-      },
-      editarProduto(produto) {
-        this.$refs.productManager.openDialog(true);
-        this.$nextTick(() => {
-          this.$refs.productManager.produto = { ...produto };
-        });
-      },
-      openDeleteDialog(produto) {
-        this.selectedProduto = produto;
-        this.deleteDialog = true;
-      },
-      closeDeleteDialog() {
-        this.deleteDialog = false;
-        this.selectedProduto = null;
-      },
-      deletarProduto() {
-        if (this.selectedProduto) {
-          this.loading = true;
-          this.deleteProduto(this.selectedProduto.id)
-            .then(() => {
-              this.closeDeleteDialog();
-            })
-            .catch(error => {
-              console.error('Erro ao excluir o produto:', error);
-              if (error.response && error.response.status === 401) {
-                this.$router.push('/login');
-                alert('Sessão expirada, por favor faça login novamente.');
-              } else {
-                alert('Erro ao excluir o produto. Por favor, tente novamente.');
-              }
-            })
-            .finally(() => {
-              this.loading = false;
-            });
-        }
+    closeDeleteDialog() {
+      this.deleteDialog = false;
+      this.selectedProduto = null;
+    },
+    deletarProduto() {
+      if (this.selectedProduto) {
+        this.loading = true;
+        this.deleteProduto(this.selectedProduto.id)
+          .then(() => {
+            this.closeDeleteDialog();
+          })
+          .catch(error => {
+            console.error('Erro ao excluir o produto:', error);
+            if (error.response && error.response.status === 401) {
+              this.$router.push('/login');
+              alert('Sessão expirada, por favor faça login novamente.');
+            } else {
+              alert('Erro ao excluir o produto. Por favor, tente novamente.');
+            }
+          })
+          .finally(() => {
+            this.loading = false;
+          });
+      }
+    }
+  },
+  filters: {
+    formatDate(value) {
+      if (value) {
+        return new Intl.DateTimeFormat('pt-BR').format(new Date(value));
       }
     },
-    filters: {
-      formatDate(value) {
-        if (value) {
-          return new Intl.DateTimeFormat('pt-BR').format(new Date(value));
-        }
-      },
-      currency(value) {
-        if (value != null) {
-          return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-        }
-        return '';
-      },
+    currency(value) {
+      if (value != null) {
+        return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+      }
+      return '';
     },
-    created() {
-      this.fetchProdutos();
-    },
-  };
-  </script>
+  },
+  created() {
+    this.fetchProdutos();
+  },
+};
+</script>
   
-  <style scoped>
-  .action-button {
-    text-transform: none;
-    font-weight: normal;
-  }
-  .font-weight-normal {
-    font-weight: normal;
-  }
-  .cancel-button {
-    border: 1px solid #006f9b;
-    color: white;
-    background-color: #006f9b;
-    text-transform: none;
-    font-weight: normal;
-  }
+<style scoped>
+.action-button {
+  text-transform: none;
+  font-weight: normal;
+}
 
-  .cancel-button:hover {
-    border: 1px solid #0081b8;
-    color: white;
-    background-color: #0081b8;
-    text-transform: none;
-    font-weight: normal;
-  }
-  .delete-button {
-    border: 1px solid #a2a9ad;
-    color: #58595b;
-    background-color: transparent;
-    text-transform: none;
-    font-weight: normal;
-  }
-  
-  .delete-button:hover {
-    border: 1px solid #a2a9ad;
-    color: #58595b;
-    background-color: transparent;
-    text-transform: none;
-    font-weight: normal;
-  }
-  
-  .delete-button:active {
-    border: 1px solid #a2a9ad;
-    color: #58595b;
-    background-color: transparent;
-    text-transform: none;
-    font-weight: normal;
-  }
-  </style>
+.font-weight-normal {
+  font-weight: normal;
+}
+
+.cancel-button {
+  border: 1px solid #006f9b;
+  color: white;
+  background-color: #006f9b;
+  text-transform: none;
+  font-weight: normal;
+}
+
+.cancel-button:hover {
+  border: 1px solid #0081b8;
+  color: white;
+  background-color: #0081b8;
+  text-transform: none;
+  font-weight: normal;
+}
+
+.delete-button {
+  border: 1px solid #a2a9ad;
+  color: #58595b;
+  background-color: transparent;
+  text-transform: none;
+  font-weight: normal;
+}
+
+.delete-button:hover {
+  border: 1px solid #a2a9ad;
+  color: #58595b;
+  background-color: transparent;
+  text-transform: none;
+  font-weight: normal;
+}
+
+.delete-button:active {
+  border: 1px solid #a2a9ad;
+  color: #58595b;
+  background-color: transparent;
+  text-transform: none;
+  font-weight: normal;
+}
+</style>
